c2567b14ae57e5713d76c932741d1fe32e1b9bd9
Zdrmokosic <xcountersmurf@gmail.com>
2020-09-28 17:21:50 +0200

    Initial Commit
    Added Unity to Github

---
+++ SuckIt/Assets/Scripts/Spawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] Trash trash;
+    [SerializeField] float z_Spawn_Offset;
+    [Tooltip("In Seconds")][SerializeField] float time_Until_Next_Spawn = 1f;
+    [SerializeField] float spawn_Radius = 15f;
+    private bool isAlive = true;
+   // Dictionary<Trash, Vector3> instantitedTrash = new Dictionary<Trash, Vector3>();
+
+    void Start()
+    {
+        StartCoroutine(SpawnTrash()); // Begins the spawning of trash
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator SpawnTrash()
+    {
+        while(isAlive) // Spawns trash while 
+        {
+            Vector3 spawnPos = GetSpawnPosition();
+            Trash spawnedTrash = Instantiate(trash, spawnPos, Quaternion.identity);
+           // instantitedTrash.Add(spawnedTrash, spawnPos);
+           // foreach(var value in instantitedTrash)
+           // {
+           //    
+           //    
+           // }
+            yield return new WaitForSeconds(time_Until_Next_Spawn); // wait for time_Until_Next_Spawn and start over again
+        }
+    }
+
+    private Vector3 GetSpawnPosition() // returns spawn position
+    {
+        z_Spawn_Offset = Random.Range(-spawn_Radius, spawn_Radius);
+        Vector3 spawnerPos = transform.position;
+        Vector3 spawnPos = new Vector3(spawnerPos.x, spawnerPos.y, spawnerPos.z + z_Spawn_Offset);
+        return spawnPos;
+    }
+
+
+   
+
+}
---
+++ SuckIt/Assets/Scripts/Trash.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    [Tooltip("0 for no speed, 1 for max speed")][SerializeField] float movingSpeedPercentage = 1f;
+    Vector3 movingDir;
+    void Start()
+    {
+        movingDir = new Vector3(transform.position.x, 0f, 0f);
+    }
+    void Update()
+    {
+        transform.position += movingDir * movingSpeedPercentage * Time.deltaTime;
+    }
+}
